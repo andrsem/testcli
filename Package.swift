2eb3bcee0d76c537bff98da804d13ffbe2bc89d2
Andrii Sem <andriisem0@gmail.com>
2025-10-20 18:58:05 +0300

    update

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "testcli",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "testcli",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-        .testTarget(name: "testcliTests")
-    ]
+  name: "testcli",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "testcli",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ]
+    ),
+    .testTarget(name: "testcliTests"),
+  ]
 )
--- Sources/testcliTests/TestCliTests.swift
+++ Sources/testcliTests/TestCliTests.swift
@@ -4,10 +4,10 @@ import Testing
 
 @Test
 func `test 1`() {
-   #expect(1 == 1)
+  #expect(1 == 1)
 }
 
 @Test
 func `test 2`() {
-   #expect(2 == 2)
+  #expect(2 == 2)
 }
